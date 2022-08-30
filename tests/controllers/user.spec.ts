bf5dd8a7fe3b4971142f533ccbab4ccd52e912f8
Jeferson <fejedamas@hotmail.com>
2022-08-30 01:41:53 -0300

    started testing (JEST)

--- tests/controllers/user.spec.ts
+++ tests/controllers/user.spec.ts
@@ -1,13 +1,52 @@
+import { Request } from 'express';
+import UserService from '../../src/services/user-service';
+import UserController from "../../src/controllers/user-controller";
+
+const makeSut = () => new UserController();
+
 describe('User Controller', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   describe('index', () => {
-    it('should show all users when accesing user route', () => {
+    it('should return all users when accesing user route', async () => {
+      const sut = makeSut();
+      const user = [{
+        id: 1,
+        name: 'any name',
+        email: 'any@mail',
+        password: '1234'
+      }];
+
+      jest.spyOn(UserService.prototype, 'find').mockResolvedValue(user);
+
+      const req: any = {} as Request;
+      const res: any = {
+        json: jest.fn().mockResolvedValue(user),
+        status: jest.fn()
+      };
+
+      const resController = await sut.index(req, res);
+
       expect.assertions(1);
-      
-      expect(1).toEqual(1);
+      expect(resController).toEqual(user);
     });
   });
-  describe('show', () => { });
-  describe('store', () => { });
-  describe('update', () => { });
-  describe('delete', () => { });
+
+  describe('show', () => {
+
+  });
+
+  describe('store', () => {
+
+  });
+
+  describe('update', () => {
+
+  });
+
+  describe('delete', () => {
+
+  });
 });
